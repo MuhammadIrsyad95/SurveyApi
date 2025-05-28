fa8cbf1abfeb966dfa1c3ef3909bf5991612edce
Muhammad Irsyad <muhammad.irsyad@kalventis.com>
2025-05-28 16:42:38 +0700

    Update : CORS

--- Program.cs
+++ Program.cs
@@ -17,7 +17,8 @@ builder.Services.AddCors(options =>
         policy.WithOrigins(
             "http://localhost:3000",
             "http://localhost:3001",
-            "http://localhost:3002"
+            "http://localhost:3002",
+            "http://localhost:8080"
         )
         .AllowAnyHeader()
         .AllowAnyMethod();
